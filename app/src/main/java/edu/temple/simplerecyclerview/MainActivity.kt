b89926456cabc50de6b49d94f930dca612adac8c
joue-zero <elnagaryoussef864@gmail.com>
2024-09-24 15:18:18 -0400

    Add View Cycle Design Pattern

--- app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
@@ -2,6 +2,8 @@ package edu.temple.simplerecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
@@ -13,8 +15,13 @@ class MainActivity : AppCompatActivity() {
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
 
         //TODO Step 2: Provide a LayoutManager
-
+        recyclerView.layoutManager = LinearLayoutManager(this)
 
         //TODO Step 4: Provide a RecyclerView.Adapter
+
+        // make array of multiple of fives and remove the zero
+        val numbers = IntArray(20) { (it+1) * 5 }
+        recyclerView.adapter = NumberDisplayAdapter(numbers)
+
     }
 }
--- app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
@@ -1,13 +1,37 @@
 package edu.temple.simplerecyclerview
 
+import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 // Step 3: Complete adapter implementation
 
-class NumberDisplayAdapter /* TODO Step 3a: Provide Adapter Parent */ {
+class NumberDisplayAdapter (private val numbers: IntArray) :
+    RecyclerView.Adapter<NumberDisplayAdapter.NumberViewHolder>() {
 
+    // holder is a class that holds the view
     class NumberViewHolder (val textView: TextView) : RecyclerView.ViewHolder (textView) {}
+
+    // create a new view holder
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = NumberViewHolder(
+        TextView(parent.context).apply { setPadding(10, 10, 10, 10) }
+    )
+
+    // populate the view holder with data
+    override fun onBindViewHolder(holder: NumberViewHolder, position: Int) {
+        holder.textView.text = numbers[position].toString()
+        holder.textView.textSize = numbers[position].toFloat()
+    }
+
+    // the getView in the adapter without recycle view, you were doing the both of these things in the getView
+
+//    override fun getItemCount(): Int {
+//        return numbers.size
+//    }
+    override fun getItemCount() = numbers.size
+    // you can reduce your function to this, this is a single expression function
+    // the interpreter see the expression as just a value
+    // 2 - 1    the compiler sees this as 1
 
     // TODO Step 3b: Complete function definitions for adapter
 
